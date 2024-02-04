9212cf1508cf8445369066b7364669b843bdf632
Soojungkim <soojung.kimm@gmail.com>
2024-02-04 14:53:22 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+const superbowlWin = record =>{
+    const winningYear = record.find(record => record.result === 'W');
+    if(winningYear){
+        return winningYear.year
+    }
+    else{
+        return undefined
+    }
+}
